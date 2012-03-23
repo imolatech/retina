c2f768a4c97f81b3787a9a01f37d6a7c17aff68b
Wenhu <guanwh@gmail.com>
2012-03-22 23:55:21 -0700

    compile settings

---
+++ retina-server/src/main/java/com/imolatech/retina/VersionInfo.java
@@ -0,0 +1,26 @@
+package com.imolatech.retina;
+
+//VersionInfo.java
+//Print OpenNI version.
+
+import org.OpenNI.*;
+
+public class VersionInfo
+{
+public static void main(String args[])
+{
+ try {
+   Context context = new Context();
+   Version vers = context.getVersion();
+   System.out.println("OpenNI v." + 
+         vers.getMajor() + "." + vers.getMinor() + "." +
+         vers.getMaintenance() + "." + vers.getBuild()); 
+   context.release();
+ } 
+ catch (GeneralException e) {
+   System.out.println(e);
+ }
+}  // end of main()
+
+} // end of VersionInfo class
+
